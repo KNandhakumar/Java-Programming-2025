acb969db5d922c7539debcb0f3e23233106241b9
Nandhakumar <nknandhakumar64@gmail.com>
2025-02-26 13:01:45 +0530

    Primitives

---
+++ src/com/NK/Primitives.java
@@ -0,0 +1,12 @@
+package com.NK;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo = 01; // 4 bytes
+        char initial = 'k';
+        float marks = 99.9f; // 4 bytes
+        double largeDecimalNumber = 1234.123; // 8 bytes
+        long largeIntValues = 123456789L; // 8 bytes
+        boolean check = true;
+    }
+}
